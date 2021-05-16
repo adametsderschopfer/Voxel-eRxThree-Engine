f34e578dc28d32074f168b329aeefd9d53c6ad42
Adamets Vladislav <v.adamets@consult-info.ru>
2021-05-17 00:52:26 +0200

    add: init gulp

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    electron: '6.0',
+                },
+            },
+        ],
+    ],
+    plugins: [
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ],
+};
