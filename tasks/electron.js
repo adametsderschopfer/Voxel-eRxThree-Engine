06668ac3ec40dee21542d7c31395cc33d8ead3a1
Adamets Vladislav <v.adamets@consult-info.ru>
2021-05-17 22:08:29 +0200

    chore: finish config gulp

---
+++ tasks/electron.js
@@ -0,0 +1,24 @@
+const electron = require('electron');
+const { spawn } = require('child_process');
+
+let subprocess;
+
+function startElectron(done) {
+  subprocess = spawn(electron, ['.', '--no-sandbox'], {
+    env: { ...process.env, NODE_ENV: 'development' },
+    stdio: 'inherit',
+  });
+
+  done();
+}
+
+function stopElectron() {
+  subprocess.kill();
+  return subprocess;
+}
+
+startElectron.displayName = 'start-electron';
+stopElectron.displayName = 'stop-electron';
+
+exports.start = startElectron;
+exports.stop = stopElectron;
